0575449ea77c0f7eca892800774d45692d16c26b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-06-02 16:19:16 +0100

    Add Flags and update seed data to match

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,9 @@
 class Country < ApplicationRecord
+  # install imagemagick
+  # sudo apt-get install imagemagick on linux or brew on Mac
+  # convert xx.png -colorspace gray xx.png
+  # convert ireland.png -resize 16x16 ./../16/ireland.png
+
   validates :name, presence: true, uniqueness: true
   validates :flag_name, presence: true
 
